3834c2a676711041cfce66d3ec7d060374e336b6
yaolunwei <995019397@qq.com>
2024-04-23 00:59:01 +0800

    开发BackgroundView

--- app/src/main/java/com/bigoat/element/sample/SidebarActivity.java
+++ app/src/main/java/com/bigoat/element/sample/SidebarActivity.java
@@ -5,12 +5,15 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bigoat.element.sample.databinding.SidebarActivityBinding;
+
 public class SidebarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sidebar_activity);
+        SidebarActivityBinding binding = SidebarActivityBinding.inflate(getLayoutInflater(), null, false);
+        setContentView(binding.getRoot());
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
 
